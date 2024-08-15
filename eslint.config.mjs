8d6a32a011e969ae59efc4e5145929bfa0992d98
Willian Silva Ferreira <112612301+NailliwFerreir@users.noreply.github.com>
2024-08-15 16:26:05 -0300

    fix: ignore some files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,26 +8,28 @@ export default [
   { languageOptions: { globals: globals.browser } },
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  { rules: {
-    semi: ['error', 'never'],
-    quotes: ['error', 'single'],
-    'no-trailing-spaces': 'error',
-    'require-jsdoc': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'indent': ['error', 2],
-    'object-curly-spacing': ['error', 'always'],
-    'linebreak-style': ['error', 'unix'],
-    'react/jsx-wrap-multilines': [
-      'error',
-      {
-        declaration: 'parens-new-line',
-        assignment: 'parens-new-line',
-        return: 'parens-new-line',
-        arrow: 'parens-new-line',
-        condition: 'parens-new-line',
-        logical: 'parens-new-line',
-        prop: 'parens-new-line',
-      },
-    ],
-  } }
+  {
+    ignorePatterns: ['*.config.js'],
+    rules: {
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+      'no-trailing-spaces': 'error',
+      'require-jsdoc': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'indent': ['error', 2],
+      'object-curly-spacing': ['error', 'always'],
+      'linebreak-style': ['error', 'unix'],
+      'react/jsx-wrap-multilines': [
+        'error',
+        {
+          declaration: 'parens-new-line',
+          assignment: 'parens-new-line',
+          return: 'parens-new-line',
+          arrow: 'parens-new-line',
+          condition: 'parens-new-line',
+          logical: 'parens-new-line',
+          prop: 'parens-new-line',
+        },
+      ],
+    } }
 ]
