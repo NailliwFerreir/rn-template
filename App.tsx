8eee419beb82920b1fdd6561f367b2772619b21d
Willian Silva Ferreira <112612301+NailliwFerreir@users.noreply.github.com>
2024-08-15 16:31:58 -0300

    fix: duplicated import

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import "@/global.css";
 import Routes from '@src/navigation/RootNavigator';
-import './global.css';
 
 export default function App() {
   return (
